298cc88c271c2a688c5b5fbcaafdff59d27a5089
Manoah Tervoort <149895ja@gmail.com>
2024-05-22 14:01:55 +0200

    chore: cleanup

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -10,7 +10,7 @@
 	viewBox="0 0 192 42"
 	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
-	class={color ? `text-${color}-500` : ''}
+	style:color
 >
 	<rect width="100%" height="100%" viewBox="0 0 192 42" fill="none" />
 	<g fill="currentColor">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,11 @@
 	import { MetaTags } from 'svelte-meta-tags';
 	import { page } from '$app/stores';
 	import extend from 'just-extend';
-	import { browser } from '$app/environment';
-	import { darkMode } from '$stores';
 
 	export let data;
 
 	$: metaTags = extend(true, {}, data.baseMetaTags, $page.data.pageMetaTags);
 	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
-
-	if (browser) {
-		darkMode.set(document.documentElement.classList.contains('dark'));
-	}
 </script>
 
 <MetaTags {...metaTags} />
