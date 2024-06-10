6c855d0ed68d73d301a2d02782591fd732509500
Manoah Tervoort <149895ja@gmail.com>
2024-06-10 14:05:45 +0200

    chore: compass

--- apps/web/src/lib/components/Arrow.svelte
+++ apps/web/src/lib/components/Arrow.svelte
@@ -2,7 +2,7 @@
 	import { onDestroy, onMount } from 'svelte'
 
 	// Utils
-	import { getBearing } from '$lib'
+	import { getBearing, getCompassHeading } from '$lib'
 	import type { Coords } from '$types'
 	import { browser } from '$app/environment'
 
@@ -11,11 +11,13 @@
 
 	const bearing = getBearing(from, to)
 	let rotation = 0
+	let north = 0
 
 	$: angle = (bearing + rotation + 360) % 360
 
 	function handleOrentation(e: DeviceOrientationEvent) {
 		rotation = e.alpha || 0
+		north = getCompassHeading(e.alpha || 0, e.beta || 0, e.gamma || 0)
 	}
 
 	onMount(() => {
@@ -37,3 +39,4 @@
 	width="50"
 	style={`transform: rotate(${angle}deg)`}
 />
+<p>{north}</p>
