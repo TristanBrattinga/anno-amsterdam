9a491da52e3ed6d190d3d8b6750e79d51aef1754
Manoah Tervoort <149895ja@gmail.com>
2024-06-12 15:09:03 +0200

    chore: add building type

--- apps/web/src/lib/types/Building.d.ts
+++ apps/web/src/lib/types/Building.d.ts
@@ -10,13 +10,20 @@ export interface Image {
 	is_main: boolean
 }
 
+export type BuildingType =
+	| 'residential'
+	| 'industrial'
+	| 'commercial'
+	| 'educational'
+	| 'recreational'
+
 export interface Building {
 	id: number
 	location: Location
 	name: string
 	address: string
 	construction_year: number
-	type_of_user?: string
+	type_of_user?: BuildingType
 	tags: Record<string, string[]>
 	description?: string
 	image_urls: Image[]
