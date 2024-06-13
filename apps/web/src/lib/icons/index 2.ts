2e2bde4e49f1d98af0ba00bdb9f1a92e7fc49464
KaoriYes <Quinten.kok@hva.nl>
2024-06-13 13:40:14 +0200

    chore: rmv dubbles

--- apps/web/src/lib/icons/index 2.ts
+++
@@ -1,5 +0,0 @@
-export { default as Logo } from './Logo.svelte';
-export { default as AudioGuideIcon } from './AudioGuideIcon.svelte';
-export { default as RouteIcon } from './RouteIcon.svelte';
-export { default as LocationIcon } from './LocationIcon.svelte';
-export { default as CloseIcon } from './CloseIcon.svelte';
